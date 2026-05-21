3ef7101a10501f073adbfd022f577aa63e63dd17
hsaest <jianx0321@gmail.com>
2026-05-21 02:36:33 -0400

    Remove with_local_model eval and rename Mind2Web2_origion to Mind2Web2

--- evaluation/Mind2Web2/Mind2Web2_with_local_model/mind2web2/api_tools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .tool_arxiv import ArxivTool
-from .tool_googlemap import GoogleMapsTool
-from .tool_pdf import PDFParser
-
-__all__ = [
-    "ArxivTool",
-    "GoogleMapsTool", 
-    "PDFParser"
-]
